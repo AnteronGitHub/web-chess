0b5dd5ca77026b24c5bda41257f3ea84e2337a90
anterongithub <anterongugl@gmail.com>
2017-07-22 13:51:40 +0300

    Added white square coloring.

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,9 +3,15 @@ import React, { Component } from 'react';
 import '../styles/square.css';
 
 export default class Square extends Component {
+    isWhite() {
+        const col = 'abcdefgh'.indexOf(this.props.file) + 1
+        const row = this.props.rank
+        return row % 2 === 1 ? col%2===0 : col%2===1
+    }
+
     render() {
         return (
-            <div className="square">
+            <div className={"square " + ((this.isWhite() ? "white" : "black"))}>
                 {this.props.file}{this.props.rank}
             </div>
         );
