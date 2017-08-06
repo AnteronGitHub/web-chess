c94adc57e212abdbd08dcdbfcd7f8664b55ce349
anterongithub <anterongugl@gmail.com>
2017-08-06 16:01:59 +0300

    Added click handling to redux game reducer.

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -1,8 +1,39 @@
 
 import Chess from 'chess'
 
+function handleClick(state, square) {
+    const selectedSquare = state.client.game.board.getSquare(square);
+    const currentSide = state.client.game.getCurrentSide();
+
+    if(state.selectedSquare===square)
+        return {
+            client: state.client,
+            selectedSquare: null
+        };
+    if(selectedSquare.piece
+            && selectedSquare.piece.side === currentSide)
+        return {
+            client: state.client,
+            selectedSquare: square
+        };
+    try {
+        state.client.move(state.selectedSquare, selectedSquare.file + selectedSquare.rank)
+        return {
+            client: state.client,
+            selectedSquare: null
+        }
+    } catch (error) {
+        if(!error.includes("Move is invalid"))
+            throw error
+    }
+
+    return state;
+}
+
 export default (state = {client: Chess.createSimple(), selectedSquare: null}, action) => {
     switch(action.type) {
+        case 'CLICK_SQUARE':
+            return handleClick(state, action.square);
         default:
             return state;
     }
